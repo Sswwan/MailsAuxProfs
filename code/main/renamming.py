bfc0d9ee7c7833da735673c19c53cc5f249d20f7
swans <swandriant@gmail.com>
2024-09-03 21:54:01 +0200

    Modification de la fenêtre pour que ce soit beau ^^

--- code/main/renamming.py
+++ code/main/renamming.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+import tkinter.filedialog
 from tkinter import *
 
 #------------------------------------------------------------------
@@ -93,38 +94,50 @@ def get_liste_fichiers(where, info_repo):
     """
     return os.listdir(get_path_files(where, info_repo))
 
+
 class InfosRepositoriesWindow:
     """
     C'est ici qu'on va stocker les chemins des différents fichiers et toutes les infos nécéssaires a plein de trucs
     """
 
     def __init__(self):
+        text = "Choisir le fichier ..."
+
+        self.extension = "None"
+        self.path_scan = "None"
+        self.path_base = "None"
+        self.path_classes = "None"
+        self.path_traitement = "None"
 
         self.w = Tk()
+        self.extension = StringVar()
         self.frame = Frame(self.w, borderwidth=2, relief = GROOVE)
 
-        self.label_scan = Label(self.frame, text = "Chemin des fichiers a renommer :\n\n")
-        self.path_scan = StringVar()
-        self.entry_scan = Entry(self.frame, textvariable=self.path_scan, width=100)
+        self.label_scan = Label(self.frame, text = "Chemin des fichiers a renommer :")
+        self.button_scan = Button(self.frame, text = text, command =lambda:self.find_directory("scan"))
+        Label(self.frame, text="\n\n")
 
-        self.label_base = Label(self.frame, text="Chemin des bases de données :\n\n")
-        self.path_base = StringVar()
-        self.entry_base = Entry(self.frame, textvariable=self.path_base, width=100)
 
-        self.label_classes = Label(self.frame, text="Chemin du répertoire contenant les fichiers classes :\n\n")
-        self.path_classes = StringVar()
-        self.entry_classes = Entry(self.frame, textvariable=self.path_classes, width=100)
+        self.label_base = Label(self.frame, text="Chemin des bases de données :")
+        self.button_base = Button(self.frame, text = text, command =lambda:self.find_directory("base"))
+        Label(self.frame, text="\n\n")
 
-        self.label_traitement = Label(self.frame, text="Chemin d'un répertoire vide (utilisé pour faire transiter des fichiers) :\n\n")
-        self.path_traitement = StringVar()
-        self.entry_traitement = Entry(self.frame, textvariable=self.path_traitement, width=100)
+        self.label_classes = Label(self.frame, text="Chemin du répertoire contenant les fichiers classes :")
+        self.button_classes = Button(self.frame, text = text, command =lambda:self.find_directory("classe"))
+        Label(self.frame, text="\n\n")
 
-        self.label_extension = Label(self.frame, text="Préciser l'extension des fichiers (avec le point devant) ! :\n\n")
-        self.extension = StringVar()
-        self.extension.set(".")
+        self.label_traitement = Label(self.frame, text="Chemin d'un répertoire vide (utilisé pour faire transiter des fichiers) :")
+        self.button_traitement = Button(self.frame, text = text, command =lambda:self.find_directory("renamed"))
+        Label(self.frame, text="\n\n")
+
+
+        self.label_extension = Label(self.frame, text="Préciser l'extension des fichiers (avec le point devant !) :")
         self.entry_extension = Entry(self.frame, textvariable=self.extension, width=100)
+        Label(self.frame, text="\n\n")
+
 
         self.b = Button(self.frame, text="Lancer le programme", command=self.frame.quit)
+        Label(self.frame, text="\n\n")
 
         self.frame.pack(side = LEFT, padx = 30, pady = 30)
         for e in self.frame.winfo_children():
@@ -132,16 +145,31 @@ class InfosRepositoriesWindow:
         self.w.mainloop()
 
 
+    def find_directory(self, type_of_file):
+
+        directory = tkinter.filedialog.askdirectory(mustexist= True)
+
+        match type_of_file:
+            case "scan":
+                self.path_scan = directory
+            case "base":
+                self.path_base = directory
+            case "classe":
+                self.path_classes = directory
+            case "renamed":
+                self.path_traitement = directory
+
+
     def get_path_scan(self):
-        return self.path_scan.get()
+        return self.path_scan
     def get_path_base(self):
-        return self.path_base.get()
+        return self.path_base
     def get_path_classes(self):
-        return self.path_classes.get()
+        return self.path_classes
     def get_path_traitement(self):
-        return self.path_traitement.get()
+        return self.path_traitement
     def get_extension(self):
-        return self.extension.get()
+        return self.extension
 
 def execute():
     infos = InfosRepositoriesWindow()
